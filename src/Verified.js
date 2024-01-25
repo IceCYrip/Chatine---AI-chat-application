004a52d34052fe339ba588f6fc4ba83f9f705ec0
IceCYrip <karansable16@gmail.com>
2024-01-25 20:33:36 +0530

    Verified page added

---
+++ src/Verified.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import "./styles/Verified.css";
+import { useNavigate } from "react-router-dom";
+
+const Verified = () => {
+  const routeTo = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      routeTo("/login");
+    }, 5000);
+  }, []);
+
+  return (
+    <div className="verifiedWrapper">
+      <div className="verifiedBody">
+        <img src="/SuccessfulTick.png" alt="greenTick" width={75} />
+        <h2>EMAIL VERIFIED</h2>
+
+        <p>
+          Thanks for verifying your email. Redirecting to loging page shortly...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Verified;
