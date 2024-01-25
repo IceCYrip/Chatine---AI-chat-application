e417049f7448e5f91dd5a84fd9492ce23f654139
IceCYrip <karansable16@gmail.com>
2024-01-25 20:33:25 +0530

    Reset Password API Integrated half

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,61 +1,68 @@
-import React, { useEffect } from 'react'
-import './styles/ResetPassword.css'
+import React, { useEffect, useState } from "react";
+import "./styles/ResetPassword.css";
 
-import TextField from './components/TextField'
-import Button from './components/Button'
-import { useNavigate, useParams } from 'react-router-dom'
+import TextField from "./components/TextField";
+import Button from "./components/Button";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ResetPassword = () => {
-  const { id } = useParams()
+  const { id } = useParams();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   useEffect(() => {
-    console.log('id:', id)
-  }, [id])
+    !!id && setIsLoggedIn(true);
+  }, [id]);
 
-  const routeTo = useNavigate()
+  const routeTo = useNavigate();
   return (
-    <div className='resetPasswordWrapper'>
-      <div className='resetPasswordBody'>
+    <div className="resetPasswordWrapper">
+      <div className="resetPasswordBody">
         <h2>Reset Password</h2>
-
+        {!isLoggedIn && (
+          <TextField
+            fullwidth
+            label="Old Password"
+            onChange={(e) => setOldPassword(e.target.value)}
+          />
+        )}
         <TextField
           fullwidth
-          label='Old Password'
-          onChange={(e) => console.log('Username: ', e.target.value)}
+          label="New Password"
+          onChange={(e) => setNewPassword(e.target.value)}
         />
         <TextField
           fullwidth
-          label='New Password'
-          onChange={(e) => console.log('Username: ', e.target.value)}
-        />
-        <TextField
-          fullwidth
-          label='Confirm New Password'
-          onChange={(e) => console.log('Password: ', e.target.value)}
-          type='password'
+          label="Confirm New Password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          type="password"
         />
 
         <Button
-          style={{ width: '100%', marginTop: '25px', fontSize: 'large' }}
+          style={{ width: "100%", marginTop: "25px", fontSize: "large" }}
           onClick={() => {}}
         >
           Reset Password
         </Button>
 
         <Button
-          color='secondary'
+          color="secondary"
           style={{
-            width: '50%',
-            marginTop: '15px',
-            fontSize: 'medium',
+            width: "50%",
+            marginTop: "15px",
+            fontSize: "medium",
           }}
-          onClick={() => routeTo('/sign-up')}
+          onClick={() => routeTo("/sign-up")}
         >
           Cancel
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResetPassword
+export default ResetPassword;
