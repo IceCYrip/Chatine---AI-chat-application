bdd9b469f9585619bd734a3f39cf71dbe58c09c0
IceCYrip <karansable16@gmail.com>
2024-02-04 19:10:57 +0530

    New Token added

--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,7 @@
 // const urls = 'http://localhost:5000' // Local
 const urls = 'https://chatine-backend.vercel.app' // Production
 
-export const replicateKey = 'Token r8_I2weMtVUs1zB1cyMnmJU540BZaBIzrF29j0of'
+// export const replicateKey = 'Token r8_I2weMtVUs1zB1cyMnmJU540BZaBIzrF29j0of'
+export const replicateKey = 'Token r8_E1xxV78DjB0IGjrzgikEjm5q1ND472j1bCBQL'
 
 export default urls
