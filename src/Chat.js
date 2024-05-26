608b1dab54428846131eaf88889f2cd08ab034c5
IceCYrip <karansable16@gmail.com>
2024-05-27 00:35:47 +0530

    Chatine: loader handled

--- src/Chat.js
+++ src/Chat.js
@@ -195,8 +195,9 @@ const Chat = () => {
       .get(`${urls}/user/getUser/${userId}`)
       .then((res) => {
         setUserData(res.data)
+        setLoadingState(false)
       })
-      .catch((error) =>
+      .catch((error) => {
         Swal.fire({
           icon: 'error',
           title: 'Something went wrong',
@@ -206,7 +207,8 @@ const Chat = () => {
           },
           buttonsStyling: false,
         })
-      )
+        setLoadingState(false)
+      })
   }
 
   const updateTheProfilePic = () => {
@@ -398,22 +400,22 @@ const Chat = () => {
   return (
     <>
       {loadingState && <Loader />}
-      <div className='chatWrapper'>
+      <div className="chatWrapper">
         {/* Blue background */}
-        <div className='accentBackground' />
+        <div className="accentBackground" />
         {/* Chat's center screen */}
-        <div className='chatBody'>
+        <div className="chatBody">
           {/* Chat's Left Column */}
-          <div className='left'>
+          <div className="left">
             {/* Chat's Left Header */}
-            <div className='leftHead'>
+            <div className="leftHead">
               <img
                 src={
                   !!userData.profilePicture
                     ? userData.profilePicture
                     : '/NoProfilePhoto.png'
                 }
-                alt=''
+                alt=""
                 width={50}
                 onClick={() => {
                   setTogglePanel(true)
@@ -423,26 +425,26 @@ const Chat = () => {
               <h3>Welcome, {userData?.fullName}</h3>
             </div>
             {/* Chat's Search Bar */}
-            <div className='chatSearch'>
+            <div className="chatSearch">
               <input
-                type='text'
-                placeholder='Search username to start chatting'
+                type="text"
+                placeholder="Search username to start chatting"
                 value={searchUsername}
                 onChange={(e) => setSearchUsername(e.target.value)}
               />
               <img
-                src='/search.svg'
-                alt='search'
+                src="/search.svg"
+                alt="search"
                 width={30}
                 onClick={newConversation}
               />
             </div>
             {/* Chat List Box */}
-            <div className='chatList'>
+            <div className="chatList">
               {/* Chats*/}
               {chats?.map((chat, index) => (
                 <div
-                  className='chat'
+                  className="chat"
                   key={index}
                   onClick={() => {
                     setActiveChat(chat)
@@ -457,7 +459,7 @@ const Chat = () => {
                         ? chat.profilePicture
                         : '/NoProfilePhoto.png'
                     }
-                    alt=''
+                    alt=""
                     width={45}
                   />
                   <span>{chat.fullName}</span>
@@ -466,20 +468,20 @@ const Chat = () => {
             </div>
 
             <div
-              className='profilePanel'
+              className="profilePanel"
               style={{ left: togglePanel ? '0%' : '-101%' }}
             >
               <div>
-                <div className='profileHead'>
+                <div className="profileHead">
                   <img
-                    src='/backIcon.svg'
-                    alt=''
+                    src="/backIcon.svg"
+                    alt=""
                     width={30}
                     onClick={() => setTogglePanel(false)}
                   />
                   <h3>Profile</h3>
                 </div>
-                <div className='profilePic'>
+                <div className="profilePic">
                   <UploadProfilePhoto
                     width={100}
                     imageSrc={
@@ -492,7 +494,7 @@ const Chat = () => {
                     imageSetter={setUploadedPhoto}
                   />
                   {uploadedPhoto && (
-                    <div className='buttonGrp'>
+                    <div className="buttonGrp">
                       <Button
                         loading={uploadLoader}
                         // loading={true}
@@ -501,7 +503,7 @@ const Chat = () => {
                         Update
                       </Button>
                       <Button
-                        color='error'
+                        color="error"
                         onClick={() => setUploadedPhoto('')}
                       >
                         Cancel
@@ -509,17 +511,17 @@ const Chat = () => {
                     </div>
                   )}
                 </div>
-                <div className='fullname'>
+                <div className="fullname">
                   {toggleNameUpdater ? (
                     <>
                       <input
                         value={nameUpdater}
                         onChange={(e) => setNameUpdater(e.target.value)}
                       />
-                      <div className='iconsGrp'>
+                      <div className="iconsGrp">
                         <img
-                          src='/cross.svg'
-                          alt=''
+                          src="/cross.svg"
+                          alt=""
                           width={30}
                           style={{ cursor: 'pointer' }}
                           onClick={() => {
@@ -527,8 +529,8 @@ const Chat = () => {
                           }}
                         />
                         <img
-                          src='/tick.svg'
-                          alt=''
+                          src="/tick.svg"
+                          alt=""
                           width={30}
                           style={{ cursor: 'pointer' }}
                           onClick={updateFullName}
@@ -545,8 +547,8 @@ const Chat = () => {
                       </span>
 
                       <img
-                        src='/edit.svg'
-                        alt=''
+                        src="/edit.svg"
+                        alt=""
                         width={25}
                         style={{ cursor: 'pointer' }}
                         onClick={() => {
@@ -557,23 +559,23 @@ const Chat = () => {
                     </>
                   )}
                 </div>
-                <div className='infoText'>
+                <div className="infoText">
                   This is not your username or pin. This name will be visible to
                   your Chatine friends.
                 </div>
                 {/* <div className='about'>{userData?.about}</div> */}
                 {/* <div className='about'> */}
-                <div className='fullname'>
+                <div className="fullname">
                   {toggleAboutUpdater ? (
                     <>
                       <input
                         value={aboutUpdater}
                         onChange={(e) => setAboutUpdater(e.target.value)}
                       />
-                      <div className='iconsGrp'>
+                      <div className="iconsGrp">
                         <img
-                          src='/cross.svg'
-                          alt=''
+                          src="/cross.svg"
+                          alt=""
                           width={30}
                           style={{ cursor: 'pointer' }}
                           onClick={() => {
@@ -581,8 +583,8 @@ const Chat = () => {
                           }}
                         />
                         <img
-                          src='/tick.svg'
-                          alt=''
+                          src="/tick.svg"
+                          alt=""
                           width={30}
                           style={{ cursor: 'pointer' }}
                           onClick={updateAbout}
@@ -594,8 +596,8 @@ const Chat = () => {
                       <span>{userData?.about}</span>
 
                       <img
-                        src='/edit.svg'
-                        alt=''
+                        src="/edit.svg"
+                        alt=""
                         width={25}
                         style={{ cursor: 'pointer' }}
                         onClick={() => {
@@ -607,13 +609,13 @@ const Chat = () => {
                   )}
                 </div>
               </div>
-              <span className='logoutButton' onClick={handleLogout}>
+              <span className="logoutButton" onClick={handleLogout}>
                 Logout
               </span>
             </div>
           </div>
-          <div className='right'>
-            <div className='rightHead'>
+          <div className="right">
+            <div className="rightHead">
               {!!activeChat?.profilePicture && (
                 <>
                   <img
@@ -622,7 +624,7 @@ const Chat = () => {
                         ? activeChat?.profilePicture
                         : '/NoProfilePhoto.png'
                     }
-                    alt=''
+                    alt=""
                     width={50}
                   />
                 </>
@@ -630,7 +632,7 @@ const Chat = () => {
               <h3>{activeChat?.fullName}</h3>
             </div>
             {/* Messages Wrapper */}
-            <div className='messagesWrapper' ref={divRef}>
+            <div className="messagesWrapper" ref={divRef}>
               {messages?.map((message, index) => (
                 <>
                   {!!message.isImg ? (
@@ -642,7 +644,7 @@ const Chat = () => {
                           : 'receivedMessage'
                       }
                       src={message.content}
-                      alt=''
+                      alt=""
                       // width={50}
                     />
                   ) : (
@@ -661,13 +663,13 @@ const Chat = () => {
               ))}
             </div>
             {/* Chat's Right Footer */}
-            <div className='chatTextBox'>
+            <div className="chatTextBox">
               <svg
-                width='32px'
+                width="32px"
                 // height='30px'
-                viewBox='0 0 24 24'
+                viewBox="0 0 24 24"
                 fill={toggleImageGenerator ? 'black' : 'yellow'}
-                xmlns='http://www.w3.org/2000/svg'
+                xmlns="http://www.w3.org/2000/svg"
                 onClick={() => setToggleImageGenerator(!toggleImageGenerator)}
                 style={{
                   cursor: 'pointer',
@@ -679,17 +681,17 @@ const Chat = () => {
               >
                 <path
                   // style={{ backgroundColor: 'red' }}
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
-                  d='M23 4C23 2.34315 21.6569 1 20 1H4C2.34315 1 1 2.34315 1 4V20C1 21.6569 2.34315 23 4 23H20C21.6569 23 23 21.6569 23 20V4ZM21 4C21 3.44772 20.5523 3 20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4Z'
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M23 4C23 2.34315 21.6569 1 20 1H4C2.34315 1 1 2.34315 1 4V20C1 21.6569 2.34315 23 4 23H20C21.6569 23 23 21.6569 23 20V4ZM21 4C21 3.44772 20.5523 3 20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4Z"
                   fill={toggleImageGenerator ? 'white' : 'grey'}
                 />
                 <path
-                  d='M4.80665 17.5211L9.1221 9.60947C9.50112 8.91461 10.4989 8.91461 10.8779 9.60947L14.0465 15.4186L15.1318 13.5194C15.5157 12.8476 16.4843 12.8476 16.8682 13.5194L19.1451 17.5039C19.526 18.1705 19.0446 19 18.2768 19H5.68454C4.92548 19 4.44317 18.1875 4.80665 17.5211Z'
+                  d="M4.80665 17.5211L9.1221 9.60947C9.50112 8.91461 10.4989 8.91461 10.8779 9.60947L14.0465 15.4186L15.1318 13.5194C15.5157 12.8476 16.4843 12.8476 16.8682 13.5194L19.1451 17.5039C19.526 18.1705 19.0446 19 18.2768 19H5.68454C4.92548 19 4.44317 18.1875 4.80665 17.5211Z"
                   fill={toggleImageGenerator ? 'white' : 'grey'}
                 />
                 <path
-                  d='M18 8C18 9.10457 17.1046 10 16 10C14.8954 10 14 9.10457 14 8C14 6.89543 14.8954 6 16 6C17.1046 6 18 6.89543 18 8Z'
+                  d="M18 8C18 9.10457 17.1046 10 16 10C14.8954 10 14 9.10457 14 8C14 6.89543 14.8954 6 16 6C17.1046 6 18 6.89543 18 8Z"
                   fill={toggleImageGenerator ? 'white' : 'grey'}
                 />
               </svg>
@@ -700,7 +702,7 @@ const Chat = () => {
               </span>
               <input
                 value={messageToSend}
-                type='text'
+                type="text"
                 onKeyDown={(e) => {
                   if (e.key === 'Enter')
                     !!messageToSend && sendMessage(activeChat?.conversationID)
@@ -709,8 +711,8 @@ const Chat = () => {
               />
 
               <img
-                src='/sendIcon.svg'
-                alt=''
+                src="/sendIcon.svg"
+                alt=""
                 width={35}
                 onClick={() => sendMessage(activeChat?.conversationID)}
               />
